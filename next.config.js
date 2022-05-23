5a7a2216d1043a1b8a249e3e055b679caa7e817b
Harsh Prasad <68845548+HARSHPRASAD22@users.noreply.github.com>
2022-05-23 15:16:01 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+    images: {
+        domains: ['i.ibb.co'],
+    },
 }
-
-module.exports = nextConfig
